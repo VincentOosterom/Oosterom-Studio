8f3037b1e16f09ce7483aba481448d56dc70463a
VincentOosterom <vincent@oosteromstudio.nl>
2026-08-09 17:48:52 +0200

    change footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -103,10 +103,10 @@ function Footer() {
                     <article className="footer-column">
                         <h4>Kennisbank</h4>
                         <ul>
-                            <li><Link to="/blog/wanneer-loont-maatwerk-software">Loont maatwerk software?</Link></li>
-                            <li><Link to="/blog/wat-is-een-ai-agent">Wat is een AI-agent?</Link></li>
-                            <li><Link to="/blog/avg-fouten-mkb">AVG-fouten die je wil vermijden</Link></li>
-                            <li><Link to="blog/5-tekenen-beveiligingslek">Tekenen van een beveiligingslek</Link></li>
+                            <li><Link to="/kennisbank/wanneer-loont-maatwerk-software">Loont maatwerk software?</Link></li>
+                            <li><Link to="/kennisbank/wat-is-een-ai-agent">Wat is een AI-agent?</Link></li>
+                            <li><Link to="/kennisbank/avg-fouten-mkb">AVG-fouten die je wil vermijden</Link></li>
+                            <li><Link to="kennisbank/5-tekenen-beveiligingslek">Tekenen van een beveiligingslek</Link></li>
                             <li><Link to="/faq">Veelgestelde vragen</Link></li>
                         </ul>
                     </article>
